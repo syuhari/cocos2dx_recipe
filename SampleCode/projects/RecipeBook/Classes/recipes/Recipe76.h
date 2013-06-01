89f83a6b8d23e23cbefbc8e5412a7cae58ccfa6e
Akihiro Matsuura <matsuura@syuhari.jp>
2013-06-01 10:04:15 +0900

    add sample text on pvr

--- SampleCode/projects/RecipeBook/Classes/recipes/Recipe76.h
+++ SampleCode/projects/RecipeBook/Classes/recipes/Recipe76.h
@@ -28,9 +28,6 @@ public:
     virtual bool init();
     static cocos2d::CCScene* scene(int step);
     CREATE_FUNC(Recipe76);
-
-    //virtual void onEnter();
-    //virtual void onExit();
 };
 
 #endif /* defined(__RecipeBook__Recipe76__) */
