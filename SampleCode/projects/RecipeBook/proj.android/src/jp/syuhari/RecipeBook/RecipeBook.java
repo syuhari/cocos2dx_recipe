a2f99bac286c72774aaa94c7ac17df74f1580565
FURUKI Eiji <fullfool@gmail.com>
2013-06-19 01:15:49 +0900

    add memory status view (android)

--- SampleCode/projects/RecipeBook/proj.android/src/jp/syuhari/RecipeBook/RecipeBook.java
+++ SampleCode/projects/RecipeBook/proj.android/src/jp/syuhari/RecipeBook/RecipeBook.java
@@ -24,16 +24,21 @@ THE SOFTWARE.
 package jp.syuhari.RecipeBook;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
@@ -43,6 +48,8 @@ import android.view.KeyEvent;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
 
 public class RecipeBook extends Cocos2dxActivity implements PictureCallback{
 
@@ -52,6 +59,7 @@ public class RecipeBook extends Cocos2dxActivity implements PictureCallback{
 
     public static native void nativeMyKeyDown(int KeyCode);
 	private static native void nativeDidTakenPhoto(final int size, final byte[] pPixels);
+    private Timer mDebugTimer = null;
 	
     protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);	
@@ -81,6 +89,30 @@ public class RecipeBook extends Cocos2dxActivity implements PictureCallback{
 				}
 			}
 		};
+		
+		if (BuildConfig.DEBUG) {
+			// ContentView に RelativeLayout追加
+			FrameLayout contentView = (FrameLayout) this.findViewById(android.R.id.content);
+			ViewGroup.LayoutParams relativelayout_params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
+			RelativeLayout relative = new RelativeLayout(this);
+			relative.setLayoutParams(relativelayout_params);
+			contentView.addView(relative);
+
+			// Debug表示用のView追加
+			TextView textView = new TextView(this);
+			textView.setBackgroundColor(Color.argb(255/4, 0, 0, 0));
+			textView.setSingleLine(false);
+			textView.setTextColor(Color.WHITE);
+	    	LayoutParams lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+	    	lp.addRule(RelativeLayout.ALIGN_LEFT, RelativeLayout.TRUE);
+	    	lp.addRule(RelativeLayout.ALIGN_TOP, RelativeLayout.TRUE);
+	    	relative.addView(textView, lp);
+
+	    	// タイマーでメモリ使用状況表示
+	    	mDebugTimer = new Timer(true);
+	    	mDebugTimer.schedule(new MemoryStatusTimerTask(textView), 1000, 1000);
+		}
+
     }
 
     public Cocos2dxGLSurfaceView onCreateView() {
@@ -143,5 +175,38 @@ public class RecipeBook extends Cocos2dxActivity implements PictureCallback{
      	   }
         });
 		
+	}
+	private class MemoryStatusTimerTask extends TimerTask {
+		private Handler handler;
+		private TextView textView;
+		   
+		public MemoryStatusTimerTask(TextView textView) {
+		    handler = new Handler();
+		    this.textView = textView;
+		}
+		
+		@Override
+		public void run() {
+		    handler.post(new Runnable() {
+		    	@Override
+		        public void run() {
+					long allocated = android.os.Debug.getNativeHeapAllocatedSize()/1024;
+					long available = android.os.Debug.getNativeHeapSize()/1024;
+					long free = android.os.Debug.getNativeHeapFreeSize()/1024;
+					long d_free = Runtime.getRuntime().freeMemory()/1024;
+					long d_total = Runtime.getRuntime().totalMemory()/1024;
+
+					ActivityManager am = ((ActivityManager)getSystemService(Activity.ACTIVITY_SERVICE));
+					ActivityManager.MemoryInfo info = new ActivityManager.MemoryInfo();
+					am.getMemoryInfo(info);
+					long l_avail = info.availMem / 1024;
+					long l_threshold = info.threshold / 1024;
+          		
+					textView.setText( String.format("native:%,7d of %,7d kb (%,7d free) \ndalvik:%,7d of %,7d kb (%,7d free)\nlinux avail:%,7d th:%,7d low:%b", 
+                		allocated, available, free, d_total-d_free, d_total, d_free, l_avail, l_threshold, info.lowMemory));
+		    	  
+		        }
+		    });
+		}
 	}
 }
